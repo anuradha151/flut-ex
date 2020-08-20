15424a6eddb77e0c38b2ae7b47a15b899b18684b
Anuradha Ranasinghe <anuradha@arimaclanka.com>
2020-08-20 21:02:04 +0530

    initial commit

---
+++ android/app/src/main/java/com/anuradha/quoetes/MainActivity.java
@@ -0,0 +1,6 @@
+package com.anuradha.quoetes;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'Quote.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Anuradha', text: 'Be yourself'),
+    Quote(author: 'Usitha', text: 'Be smartest in the room'),
+    Quote(author: 'Ravindu', text: 'The truth is pure and never simple')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes
+            .map((quote) => Text('${quote.text} - ${quote.author}'))
+            .toList(),
+      ),
+    );
+  }
+}
